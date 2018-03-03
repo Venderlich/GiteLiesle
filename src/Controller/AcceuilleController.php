81a7c22f519122d4ff18a86d6eede50683ee430e
Venderlich <tristan.cuenot@laposte.net>
2018-03-03 17:40:14 +0100

    Ajout d'une premiere page du front

---
+++ src/Controller/AcceuilleController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tristan
+ * Date: 03/03/2018
+ * Time: 13:47
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AcceuilleController extends Controller
+{
+    public function getWiewAction(){
+        return $this->render('Front/Acceuil/index.html.twig');
+    }
+}
